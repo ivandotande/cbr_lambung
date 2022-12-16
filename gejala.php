60125cf83dec26f7f25dcf3e5984ac9dbe4b37ef
IT@lenovo <ivandotande@gmail.com>
2022-12-16 21:50:12 +0700

    Update Save Button on gejala

--- gejala.php
+++ gejala.php
@@ -5,7 +5,7 @@ include '.\PHP\Navbar_Admin.php';
 
 <html>
     <body>
-        <h3> Data Penyakit dan solusi Pengangannya </h3>
+        <h3> Data gejala gejala </h3>
         <div class="adminpage">
             <div class="form">  
                 <form method="post" class="admin-form" >
@@ -23,7 +23,7 @@ include '.\PHP\Navbar_Admin.php';
                             <label> Gejala </label>
                             </td>
                             <td>
-                            <input type="text" name= "gejala" required/><br>
+                            <input type="text" name= "descGejala" required/><br>
                             </td>
                         </tr>
                     </table>
@@ -32,23 +32,24 @@ include '.\PHP\Navbar_Admin.php';
                 </form>
             </div>
             <div class="table">
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form>
                     <table>
-                        <h1>Table Data Penyakit</h1>
+                        <h1>Table Data gejala</h1>
                         <thread>
                             <tr>
-                                <th>Kode Penyakit</th>
-                                <th>Nama Penyakit</th>
+                                <th>Kode Gejala</th>
+                                <th>Nama Gejala</th>
                             </tr>
                         </thread>
                         <tbody>
                         <?php
+                            $sql = mysqli_query($conn, "SELECT gejala_id, gejala_nama FROM db_gejala");
                             while ($data = mysqli_fetch_array($sql)) {
                             ?>
                                 <tr>
                                     <form>
-                                        <td><?php echo $data['penyakit_id']?></td>
-                                        <td><?php echo $data['penyakit_nama']; ?></td>
+                                        <td><?php echo $data['gejala_id']?></td>
+                                        <td><?php echo $data['gejala_nama']; ?></td>
                                     </form>
                                 </tr>
                             <?php
@@ -56,21 +57,35 @@ include '.\PHP\Navbar_Admin.php';
                             ?>
                         </tbody>
                         <?php
-                        function savefile(){
                             include '.\PHP\connection.php';
                             if (isset($_POST['save_btn'])){
-                                $kodegejala = $_GET['kGejala'];
-                                $namapenyakit = $_GET['gejala'];
-                                $updatepenyakit = "update db_gejala set gejala_nama='$namapenyakit' where gejala_id='$kodegejala'";
-                                $run_updatepenyakit = mysqli_query($conn,$updatepenyakit);
-                                if($run_updatepenyakit){
-                                    echo "<script>alert('Gejala Sudah Di update')</script>";
+                                $kodegejala = $_POST['kGejala'];
+                                $namagejala = $_POST['descGejala'];
+                                if($kodegejala == " " or $namagejala = " "){
+                                    echo "<script>alert('Empty Value')</script>";
+                                    echo "<script>alert('kode = $kodegejala dan nama = $namagejala')</script>";
                                     echo "<script>window.open('gejala.php','_self')</script>";
-                                }else{
                                     
                                 }
-                            }
-                        }
+                                else{
+                                    $find_gejala = "select * db_gejala where gejala_nama='$namagejala' and gejala_id='$kodegejala'";
+                                $check_gejala = mysqli_query($conn,$find_gejala);
+                                if($check_gejala == "0"){
+                                    $insert_gejala = "insert into db_gejala(gejala_nama,gejala_id) values ('$namagejala' ,'$kodegejala')";
+                                    $run_updategejala = mysqli_query($conn,$insert_gejeala);
+                                    echo "<script>alert('Gejala Sudah Di masukan')</script>";
+                                    echo "<script>window.open('gejala.php','_self')</script>";
+                                }
+                                elseif($check_gejala == "1"){
+                                    $update_gejala = "update db_gejala set gejala_nama='$namagejala' where gejala_id='$kodegejala'";
+                                    $run_updategejala = mysqli_query($conn,$update_gejala);
+                                    echo "<script>alert('Gejala Sudah Di update')</script>";
+                                    echo "<script>window.open('gejala.php','_self')</script>";
+                                }
+                                else{
+                                } 
+                            }  
+                        }                       
                         ?>
                     </table>
                 </form>
